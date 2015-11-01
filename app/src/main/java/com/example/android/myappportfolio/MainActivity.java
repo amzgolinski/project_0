ea337130d83654716874698dce4c8531c367eb2a
amzgolinski <amzgolinski@gmail.com>
2015-10-31 18:09:20 -0700

    Changes based on code review

--- app/src/main/java/com/example/android/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -38,21 +38,6 @@ public class MainActivity extends AppCompatActivity {
     return true;
   }
 
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
-  }
-
   public void mediaStreamerToast(View view) {
     this.showToast(getResources().getString(R.string.media_streamer_app_toast));
   }
